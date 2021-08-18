d968c5da1730386a27e3611a0344b6f019231601
katemagbitang <julianne_kristine_magbitang@dlsu.edu.ph>
2021-08-19 02:03:14 +0800

    initial recording activity layout

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s17/magbitang/julianne/soundvox/RecordingActivity.java
@@ -0,0 +1,14 @@
+package ph.edu.dlsu.mobdeve.s17.magbitang.julianne.soundvox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording);
+    }
+}
