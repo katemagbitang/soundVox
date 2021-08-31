f4fcf33c677e0da858b8a36d2abee8d51f52e6c8
katemagbitang <julianne_kristine_magbitang@dlsu.edu.ph>
2021-09-01 03:21:56 +0800

    fixed transitions and trial on profile to main

--- app/src/main/java/ph/edu/dlsu/mobdeve/s17/magbitang/julianne/soundvox/MenuProfileActivity.java
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s17/magbitang/julianne/soundvox/MenuProfileActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -11,13 +12,15 @@ import android.widget.Button;
 import ph.edu.dlsu.mobdeve.s17.magbitang.julianne.soundvox.adapters.ProfileAdapter;
 import ph.edu.dlsu.mobdeve.s17.magbitang.julianne.soundvox.database.ProfileDAO;
 import ph.edu.dlsu.mobdeve.s17.magbitang.julianne.soundvox.database.ProfileDAOSqlImpl;
+import ph.edu.dlsu.mobdeve.s17.magbitang.julianne.soundvox.models.Profile;
 
 public class MenuProfileActivity extends AppCompatActivity {
 
-    private Button back_btn;
+    private Button back_btn, profile_btn;
     private ProfileAdapter profileAdapter;
     private RecyclerView rvProfile;
     private RecyclerView.LayoutManager layout;
+    private Profile profile = new Profile();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +30,22 @@ public class MenuProfileActivity extends AppCompatActivity {
         init();
 
         back_btn = findViewById(R.id.goback_btn);
+        profile_btn = findViewById(R.id.profile_item);
+
 
         back_btn.setOnClickListener(view -> {
             Intent goToMain = new Intent(MenuProfileActivity.this, MainActivity.class);
             startActivity(goToMain);
         });
+
+//        profile_btn.setOnClickListener(view -> {
+//            Intent intent = new Intent();
+//            intent.putExtra("id", profile.getId());
+//            intent.putExtra("name", profile.getName());
+//            intent.putExtra("sound", profile.getSounds());
+//            setResult(Activity.RESULT_OK, intent);
+//            finish();
+//        });
     }
 
 
