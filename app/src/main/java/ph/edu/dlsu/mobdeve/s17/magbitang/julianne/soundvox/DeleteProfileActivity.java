986afd06a6d1fd452021b93fce6b13b8cc2be069
mcbsanchez <martin_christopher_sanchez@dlsu.edu.ph>
2021-09-02 01:57:31 +0800

    Delete compenents added

--- app/src/main/java/ph/edu/dlsu/mobdeve/s17/magbitang/julianne/soundvox/DeleteProfileActivity.java
+++ app/src/main/java/ph/edu/dlsu/mobdeve/s17/magbitang/julianne/soundvox/DeleteProfileActivity.java
@@ -8,38 +8,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-import ph.edu.dlsu.mobdeve.s17.magbitang.julianne.soundvox.adapters.SelectProfileAdapter;
+import ph.edu.dlsu.mobdeve.s17.magbitang.julianne.soundvox.adapters.DeleteProfileAdapter;
 import ph.edu.dlsu.mobdeve.s17.magbitang.julianne.soundvox.database.ProfileDAO;
 import ph.edu.dlsu.mobdeve.s17.magbitang.julianne.soundvox.database.ProfileDAOSqlImpl;
 
-public class SelectProfileActivity extends AppCompatActivity {
+public class DeleteProfileActivity extends AppCompatActivity {
 
     private Button back_btn;
-    private SelectProfileAdapter selectProfileAdapter;
+    private DeleteProfileAdapter deleteProfileAdapter;
     private RecyclerView rvProfile;
     private RecyclerView.LayoutManager layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_profile);
+        setContentView(R.layout.activity_delete_profile);
 
         init();
 
         back_btn = findViewById(R.id.goback_btn);
 
         back_btn.setOnClickListener(view -> {
-            Intent goToMain = new Intent(SelectProfileActivity.this, MenuActivity.class);
+            Intent goToMain = new Intent(DeleteProfileActivity.this, MenuActivity.class);
             startActivity(goToMain);
         });
     }
 
     private void init(){
-        this.rvProfile = findViewById(R.id.selectProfileRecyclerView);
+        this.rvProfile = findViewById(R.id.deleteProfileRecyclerView);
         this.layout = new LinearLayoutManager(this);
         this.rvProfile.setLayoutManager(this.layout);
         ProfileDAO profileDAO = new ProfileDAOSqlImpl(getApplicationContext());
-        this.selectProfileAdapter = new SelectProfileAdapter(getApplicationContext(),profileDAO.getProfiles());
-        this.rvProfile.setAdapter(this.selectProfileAdapter);
+        this.deleteProfileAdapter = new DeleteProfileAdapter(getApplicationContext(),profileDAO.getProfiles());
+        this.rvProfile.setAdapter(this.deleteProfileAdapter);
     }
 }
